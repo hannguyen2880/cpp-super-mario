66c55fc026172d396419137b9e3e15f03fe46a1a
NamHV05 <88269724+NamHV05@users.noreply.github.com>
2024-12-27 22:37:19 +0700

    add audio handling

---
+++ src/audio/AudioManager.h
@@ -0,0 +1,36 @@
+#ifndef MARIO_MAKER_AUDIOMANAGER_H
+#define MARIO_MAKER_AUDIOMANAGER_H
+#include <raylib.h>
+#include <unordered_map>
+#include "../audio/AudioIds.h"
+
+class AudioManager {
+public:
+
+    AudioManager();
+
+    ~AudioManager();
+
+    void playSound(SoundId soundId);
+
+    void playMusic(MusicId musicId);
+
+    void pauseCurrentMusic();
+
+    void resumeCurrentMusic();
+
+    void stopCurrentMusic();
+
+    Music getCurrentMusic();
+
+    bool isMusicPaused();
+
+private:
+    std::unordered_map<SoundId, Sound> soundMap_;
+    std::unordered_map<MusicId, Music> musicMap_;
+    Music currentMusic_;
+    bool pause = true;
+};
+
+
+#endif //MARIO_MAKER_AUDIOMANAGER_H
