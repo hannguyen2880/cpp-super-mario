612d5198550cea0a17f74e82de031d721f370f2a
Thinh313 <23125092@student.hcmus.edu.vn>
2024-12-27 14:25:35 +0700

    add scoreboard save

--- src/screens/ScoreboardScreen.h
+++ src/screens/ScoreboardScreen.h
@@ -32,20 +32,22 @@
  */
 
 #pragma once
+
 #include "Screen.h"
+#include <string>
 #include "../ui/Button.h"
 #include "../ui/ImageButton.h"
-#include "../game/State/MainMenuState.h"
-#include <string>
+#include "../State.h"
+
 #include <utility>
 #include <vector>
-using namespace std;
 
 
 class ScoreboardScreen : public Screen {
 private:
+
     Texture2D background;
-    vector<Texture2D> placeTextures;
+    std::vector<Texture2D> placeTextures;
     ImageButton backButton;
     float frame_positionX_1st;
     float frame_positionY_1st;
@@ -56,15 +58,14 @@ private:
     float textLeftMargin;
     float textRightMargin;
     float textVerticalSpacing;
-    vector<pair<string, int>> players; // vector of pairs of player names and scores
+    std::vector<std::pair<std::string, int>> players; // vector of pairs of player names and scores
 
 public:
-    void loadScores();
     ScoreboardScreen();
     ~ScoreboardScreen();
     void Init() override;
     void Update() override;
     void Draw() override;
     void Unload() override;
-
+    void loadScores();
 };
