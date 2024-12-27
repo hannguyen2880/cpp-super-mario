c68db8b87472d615b70cc32aafd993dae00e8b39
Thinh313 <23125092@student.hcmus.edu.vn>
2024-12-27 14:07:04 +0700

    add escalator image

--- src/Constants.h
+++ src/Constants.h
@@ -7,6 +7,7 @@ constexpr const char* SBM1_PLAYER_TILESET_PATH = "../assets/tilesets/players-til
 constexpr const char* SMB1_TILESET_PATH = "../assets/tilesets/tiles-2.png";
 constexpr const char* SMB1_ENEMIES_TILESET_PATH = "../assets/tilesets/enemies-tileset.png";
 constexpr const char* SMB1_OBJECT_TILESET_PATH = "../assets/tilesets/objects-tileset.png";
+constexpr const char* LANDSCAPE = "../assets/tilesets/landscape.jpg";
 
 constexpr const char* FONT_FILE_NAME = "../assets/fonts/SuperMario256.ttf";
 
@@ -17,6 +18,13 @@ constexpr const char* INSTRUCTION_PANEL = "../assets/imgs/InstructionPanel.png";
 constexpr const char* MARIO_MENU = "../assets/imgs/Mario.png";
 constexpr const char* RESUMEGAME_PANEL = "../assets/imgs/ResumeGamePanel.png";
 constexpr const char* MENU_BACKGROUND = "../assets/imgs/Screen_background.png";
+
+//background
+constexpr const char* AUTUMN_EASY = "../assets/tilesets/autumn_easy.png";
+constexpr const char* AUTUMN_MED = "../assets/tilesets/autumn.png";
+constexpr const char* AUTUMN_HARD = "../assets/tilesets/autumn_midnight.png";
+
+//scoreboard
 constexpr const char* SCOREBOARD_BACKGROUND = "../assets/imgs/BackgroundScoreboard.png";
 
 constexpr const char* FRIST_PLACE = "../assets/imgs/1st.png";
@@ -42,9 +50,11 @@ constexpr const char* PAUSE_BUTTON = "../assets/imgs/Buttons/PauseButton.png";
 constexpr const char* HOME_BUTTON = "../assets/imgs/Buttons/HomeButton.png";
 
 //map
-constexpr const char* HARD_MAP = "../assets/maps/MidEASY.tmx";
-constexpr const char* MEDIUM_MAP = "../assets/maps/playground_world.tmx";
-constexpr const char* EASY_MAP = "../assets/maps/map_1.tmx";
+constexpr const char* HARD_MAP = "../assets/maps/map_1.tmx";
+constexpr const char* MEDIUM_MAP = "../assets/maps/MidEASY.tmx";
+//constexpr const char* EASY_MAP = "../assets/maps/playground_world.tmx";
+constexpr const char* EASY_MAP = "../assets/maps/EASY.tmx";
+
 //......
 constexpr bool SECOND_PLAYER = false;
 
@@ -66,3 +76,5 @@ constexpr float MARIO_BOUNCE = 10.0f;
 
 constexpr float MUSHROOM_GROW_SPEED = .25f;
 constexpr float MUSHROOM_MOVE_SPEED = 0.8f;
+
+constexpr float ESCALATOR_GROW_SPEED = 0.5f;
